41fc72e5d55ae05baace18ba5ae591fddf5037fa
Huy Hoàng Ngô <ngohuyhoang1808@gmail.com>
2025-03-30 15:28:51 +0700

    Create Folder ADO, Controls, Models

--- ManageFurniture/obj/Debug/net8.0-windows/ManageFurniture.AssemblyInfo.cs
+++ ManageFurniture/obj/Debug/net8.0-windows/ManageFurniture.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManageFurniture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+861aaa9bd3493480e6129200e7911557d23f21cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5e6d3f95f7270361f30e311285a5cea6dbde80e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManageFurniture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManageFurniture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
