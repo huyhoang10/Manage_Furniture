7502d66b629102dba9884305142673dd727abf64
Huy Hoàng Ngô <ngohuyhoang1808@gmail.com>
2025-04-12 11:50:50 +0700

    modify color button

--- Manage_Furniture/Manage_Furniture/Forms/FEmployee.cs
+++ Manage_Furniture/Manage_Furniture/Forms/FEmployee.cs
@@ -17,14 +17,42 @@ namespace Manage_Furniture.Forms
             InitializeComponent();
         }
 
-        private void guna2Button1_Click(object sender, EventArgs e)
+
+        private void FEmployee_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void test1_Load(object sender, EventArgs e)
+        protected void SetAllButton()
         {
+            btnCustomer.FillColor = Color.White;
+            btnOrder.FillColor = Color.White;
+            btnWarehouse.FillColor = Color.White;
+            btnSupplier.FillColor = Color.White;
+        }
 
+        private void btnCustomer_Click(object sender, EventArgs e)
+        {
+            SetAllButton();
+            btnCustomer.FillColor = Color.FromArgb(197, 166, 158);
+        }
+
+        private void btnOrder_Click(object sender, EventArgs e)
+        {
+            SetAllButton();
+            btnOrder.FillColor = Color.FromArgb(197, 166, 158);
+        }
+
+        private void btnWarehouse_Click(object sender, EventArgs e)
+        {
+            SetAllButton();
+            btnWarehouse.FillColor = Color.FromArgb(197, 166, 158);
+        }
+
+        private void btnSupplier_Click(object sender, EventArgs e)
+        {
+            SetAllButton();
+            btnSupplier.FillColor = Color.FromArgb(197, 166, 158);
         }
     }
 }
